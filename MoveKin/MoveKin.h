cba1d0a915763b5eaa8e0d02a1dcfeca6215388d
Natan Rolnik <natanrolnik@gmail.com>
2019-01-10 16:04:42 +0200

    Initial commit

---
+++ MoveKin/MoveKin.h
@@ -0,0 +1,19 @@
+//
+//  MoveKin.h
+//  MoveKin
+//
+//  Created by Natan Rolnik on 10/01/19.
+//  Copyright © 2019 kinecosystem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoveKin.
+FOUNDATION_EXPORT double MoveKinVersionNumber;
+
+//! Project version string for MoveKin.
+FOUNDATION_EXPORT const unsigned char MoveKinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoveKin/PublicHeader.h>
+
+
